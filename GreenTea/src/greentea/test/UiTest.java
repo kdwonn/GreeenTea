2160cfcccb2ae21e6c536a09164fbfb52afb1fbf
DongwonKim <kdwon@postech.ac.kr>
2017-11-08 21:07:58 +0900

    create UI test class

---
+++ GreenTea/src/greentea/test/UiTest.java
@@ -0,0 +1,23 @@
+package greentea.test;
+
+import static org.junit.Assert.*;
+
+import org.eclipse.swtbot.eclipse.finder.SWTWorkbenchBot;
+import org.eclipse.swtbot.swt.finder.junit.SWTBotJunit4ClassRunner;
+import org.eclipse.swtbot.swt.finder.widgets.SWTBotLabel;
+import org.eclipse.swtbot.swt.finder.widgets.SWTBotMenu;
+import org.eclipse.swtbot.swt.finder.widgets.SWTBotShell;
+import org.eclipse.swtbot.swt.finder.widgets.SWTBotText;
+import org.eclipse.swtbot.swt.finder.widgets.SWTBotTree;
+import org.eclipse.swtbot.swt.finder.widgets.SWTBotTreeItem;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+@RunWith(SWTBotJunit4ClassRunner.class)
+public class UiTest {
+	private static SWTWorkbenchBot bot;
+	
+}
