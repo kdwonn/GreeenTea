7100d163b5794d5a4b0c1fe7d7a768ef23e26f97
Kim Jaechang <jaechang@postech.ac.kr>
2017-11-14 00:12:12 +0900

    add methods in ProjectAnalyser
    add method getProjectNames
    add method getProjects
    add method getPackageNames
    add method getPackages

--- GreenTea/src/greentea/ProjectAnalyser.java
+++ GreenTea/src/greentea/ProjectAnalyser.java
@@ -1,5 +1,93 @@
 package greentea;
 
+import java.util.LinkedList;
+import java.util.List;
+
+import org.eclipse.core.resources.IProject;
+import org.eclipse.core.resources.ResourcesPlugin;
+import org.eclipse.core.runtime.CoreException;
+import org.eclipse.jdt.core.ICompilationUnit;
+import org.eclipse.jdt.core.IJavaElement;
+import org.eclipse.jdt.core.IJavaProject;
+import org.eclipse.jdt.core.IPackageFragment;
+import org.eclipse.jdt.core.IPackageFragmentRoot;
+import org.eclipse.jdt.core.JavaCore;
+import org.eclipse.jdt.internal.core.JarPackageFragmentRoot;
+
 class ProjectAnalyser {
+	public String[] getProjectNames() {
+		IJavaProject[] projects = getProjects();
+		List<String> nameList = new LinkedList<String>();
+		for(IJavaProject prj : projects) {
+			nameList.add(prj.getElementName());
+		}
+		return nameList.toArray(new String[] {});
+	}
+	
+	public String[] getPackageNames(String projectName) {
+		IPackageFragment[] packages = getPackages(projectName);
+		List<String> nameList = new LinkedList<String>();
+		for(IPackageFragment pack : packages) {
+			nameList.add(pack.getElementName());
+		}
+		return nameList.toArray(new String[] {});
+	}
+	
+	public String[] getClassNames(String projectName, String packageName) {
+		//TODO
+		return null;
+	}
+	
+	public String getSouceCode(String projectName, String packageName, String ClassName) {
+		//TODO
+		return null;
+	}
+	
+	private IJavaProject[] getProjects() {
+		IProject[] projects =  ResourcesPlugin.getWorkspace().getRoot().getProjects();
+		List<IJavaProject> projectList = new LinkedList<IJavaProject>();
+		for(IProject prj : projects) {
+			IJavaProject javaProj = JavaCore.create(prj);
+			if(javaProj != null) {
+				projectList.add(javaProj);
+			}
+		}
+		return projectList.toArray(new IJavaProject[] {});
+	}
+	
+	private IPackageFragment[] getPackages(String projectName) {
+		IJavaProject[] projects = getProjects();
+		IJavaProject objectProject = null;
+		for(IJavaProject prj : projects) {
+			if(prj.getElementName().equals(projectName)) {
+				objectProject = prj;
+				break;
+			}
+		}
+		if(objectProject == null) {
+			return null;		
+		}
+		
+		IPackageFragmentRoot[] packageRoots;
+		try {
+			packageRoots = objectProject.getPackageFragmentRoots();
+			List<IPackageFragment> fragmentList = new LinkedList<IPackageFragment>();
+			for(IPackageFragmentRoot packRoot : packageRoots) {
+				if(packRoot instanceof JarPackageFragmentRoot) continue;
+	            IJavaElement[] fragments = packRoot.getChildren();
+	            for(IJavaElement fragJE : fragments) {
+	            	IPackageFragment frag = (IPackageFragment)fragJE;
+	            	if(frag.getElementName().isEmpty()) continue;
+	            	fragmentList.add(frag);
+	            }
+			}
+			return fragmentList.toArray(new IPackageFragment[] {});
+		} catch (CoreException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			return null;
+		}
+		
+	}
 	
 }
