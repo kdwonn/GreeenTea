e5ef4016f5abee099de03f073c77c225b1af4d75
ymfact <ymfact@gmail.com>
2017-11-17 18:50:54 +0900

    Implementing UI(Not completed) w/ jspak

--- GreenTea/src/greentea/GreenTea.java
+++ GreenTea/src/greentea/GreenTea.java
@@ -9,88 +9,46 @@ import org.eclipse.e4.ui.services.IServiceConstants;
 import org.eclipse.jface.viewers.ISelection;
 import org.eclipse.jface.viewers.IStructuredSelection;
 import org.eclipse.swt.SWT;
+import org.eclipse.swt.layout.GridData;
+import org.eclipse.swt.layout.GridLayout;
 import org.eclipse.swt.widgets.Composite;
 import org.eclipse.swt.widgets.Label;
+import org.eclipse.swt.widgets.Table;
+import org.eclipse.swt.widgets.Tree;
+import org.eclipse.jface.viewers.*;
+
+/*
+ * Will be changed 
+ */
 
 public class GreenTea {
-	private Label myLabelInView;
+	private org.eclipse.jface.viewers.TreeViewer treeViewer;
+	private org.eclipse.jface.viewers.TableViewer tableViewer;
 
+	private void setViewer() {
+		
+	}
+	
 	@PostConstruct
 	public void createPartControl(Composite parent) {
-		System.out.println("Enter in SampleE4View postConstruct");
+	    Composite container = new Composite(parent, SWT.NONE);
+	    GridLayout gl_container = new GridLayout(2, false);
+	    container.setLayout(gl_container);
+	    
+	    setViewer();
+	    
+	    treeViewer = new org.eclipse.jface.viewers.TreeViewer(container, SWT.BORDER | SWT.FULL_SELECTION);
+	    Tree leftTree = treeViewer.getTree();
+	    leftTree.setLayoutData(new GridData(SWT.FILL, SWT.FILL, true, true, 1, 1));
 
-		myLabelInView = new Label(parent, SWT.BORDER);
-		myLabelInView.setText("This is a sample E4 view");
-
+	    tableViewer = new org.eclipse.jface.viewers.TableViewer(container, SWT.BORDER | SWT.FULL_SELECTION);
+	    Table rightTable = tableViewer.getTable();
+	    rightTable.setLayoutData(new GridData(SWT.FILL, SWT.FILL, true, true, 1, 1));
 	}
 
 	@Focus
 	public void setFocus() {
-		myLabelInView.setFocus();
+		treeViewer.getControl().setFocus();
 
-	}
-
-	/**
-	 * This method is kept for E3 compatiblity. You can remove it if you do not
-	 * mix E3 and E4 code. <br/>
-	 * With E4 code you will set directly the selection in ESelectionService and
-	 * you do not receive a ISelection
-	 * 
-	 * @param s
-	 *            the selection received from JFace (E3 mode)
-	 */
-	@Inject
-	@Optional
-	public void setSelection(@Named(IServiceConstants.ACTIVE_SELECTION) ISelection s) {
-		if (s==null || s.isEmpty())
-			return;
-
-		if (s instanceof IStructuredSelection) {
-			IStructuredSelection iss = (IStructuredSelection) s;
-			if (iss.size() == 1)
-				setSelection(iss.getFirstElement());
-			else
-				setSelection(iss.toArray());
-		}
-	}
-
-	/**
-	 * This method manages the selection of your current object. In this example
-	 * we listen to a single Object (even the ISelection already captured in E3
-	 * mode). <br/>
-	 * You should change the parameter type of your received Object to manage
-	 * your specific selection
-	 * 
-	 * @param o
-	 *            : the current object received
-	 */
-	@Inject
-	@Optional
-	public void setSelection(@Named(IServiceConstants.ACTIVE_SELECTION) Object o) {
-
-		// Remove the 2 following lines in pure E4 mode, keep them in mixed mode
-		if (o instanceof ISelection) // Already captured
-			return;
-
-		// Test if label exists (inject methods are called before PostConstruct)
-		if (myLabelInView != null)
-			myLabelInView.setText("Current single selection class is : " + o.getClass());
-	}
-
-	/**
-	 * This method manages the multiple selection of your current objects. <br/>
-	 * You should change the parameter type of your array of Objects to manage
-	 * your specific selection
-	 * 
-	 * @param o
-	 *            : the current array of objects received in case of multiple selection
-	 */
-	@Inject
-	@Optional
-	public void setSelection(@Named(IServiceConstants.ACTIVE_SELECTION) Object[] selectedObjects) {
-
-		// Test if label exists (inject methods are called before PostConstruct)
-		if (myLabelInView != null)
-			myLabelInView.setText("This is a multiple selection of " + selectedObjects.length + " objects");
 	}
 }
--- GreenTea/src/greentea/TreeViewer.java
+++ GreenTea/src/greentea/TreeViewer.java
@@ -1,5 +1,11 @@
 package greentea;
 
-public class TreeViewer {
+import org.eclipse.jface.viewers.*;
 
+public class TreeViewer {
+	private org.eclipse.jface.viewers.TreeViewer viewer;
+	
+	public org.eclipse.jface.viewers.TreeViewer getViewer() {
+		return viewer;
+	}
 }
