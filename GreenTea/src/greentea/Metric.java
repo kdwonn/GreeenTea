07d081c2528c466879a3a0b9fc9d9a25de4d3e9d
DongwonKim <kdwon@postech.ac.kr>
2017-11-18 15:39:11 +0900

    create method prototype for Cyclomatic & Dhama with Park Namgyu

--- GreenTea/src/greentea/Metric.java
+++ GreenTea/src/greentea/Metric.java
@@ -1,5 +1,31 @@
 package greentea;
 
-public class Metric {
+import org.eclipse.jdt.core.dom.ASTVisitor;
+import org.eclipse.jdt.core.dom.AnnotationTypeDeclaration;
+import org.eclipse.jdt.core.dom.AnonymousClassDeclaration;
+import org.eclipse.jdt.core.dom.Block;
+import org.eclipse.jdt.core.dom.CatchClause;
+import org.eclipse.jdt.core.dom.ConditionalExpression;
+import org.eclipse.jdt.core.dom.DoStatement;
+import org.eclipse.jdt.core.dom.EnhancedForStatement;
+import org.eclipse.jdt.core.dom.EnumDeclaration;
+import org.eclipse.jdt.core.dom.Expression;
+import org.eclipse.jdt.core.dom.ExpressionStatement;
+import org.eclipse.jdt.core.dom.ForStatement;
+import org.eclipse.jdt.core.dom.IfStatement;
+import org.eclipse.jdt.core.dom.MethodDeclaration;
+import org.eclipse.jdt.core.dom.SwitchCase;
+import org.eclipse.jdt.core.dom.TypeDeclaration;
+import org.eclipse.jdt.core.dom.VariableDeclarationFragment;
+import org.eclipse.jdt.core.dom.WhileStatement;
 
+public class Metric {
+	static public int measureCyclomatic
+	(String projectName, String packageName, String className, String methodName) {
+		return 0;
+	}
+	static public double measureDhama
+	(String projectName, String packageName, String className, String methodName) {
+		return 0;
+	}
 }
