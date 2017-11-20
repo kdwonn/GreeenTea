d15bf6f12cdab9b31946ac568f2089a7d46a03d6
Park Junsu <jspak3@postech.ac.kr>
2017-11-20 23:22:58 +0900

    Implemented test of T_S06

--- GreenTea/src/greentea/test/MetricTest.java
+++ GreenTea/src/greentea/test/MetricTest.java
@@ -1,38 +1,43 @@
 package greentea.test;
 
-import static org.junit.Assert.assertTrue;
-
 import org.junit.Test;
+import static org.junit.Assert.*;
+import testexample.SimpleTest;
+import org.eclipse.jdt.core.*;
 
 import greentea.GreenTea;
+import greentea.Metric;
 
 public class MetricTest {
 	@Test
 	public void DhamaCouplingTest() {
 		double estimatedValue = 1/22;
-		assertEqual(estimatedValue, greentea.MeasureMetric.measureCoupling(new testexample.Bank("bank")));
+		assertEquals(estimatedValue, Metric.measureDhama()));
 	}
 	
 	@Test
 	public void MartinCouplingTest() {
 		double estimatedValue = 1;
-		assertEqual(estimatedValue, greentea.MeasureMetric.measureCoupling(new testexample.Bank("bank")));
+		assertEquals(estimatedValue, Metric.measureMartin()));
 	}
 	
 	@Test
 	public void HalsteadVolumeTest() {
 		double estimatedValue = 138.3;
-		assertEqual(estimatedValue, greentea.MeasureMetric.measureHalstead(new testexample.SimpleTest().cycle()));
+		IMethod dummy = null;
+		assertEquals(estimatedValue, Metric.measureHalstead(dummy));
 	}
 	
 	@Test
 	public void CyclomaticComplexTest() {
 		double estimatedValue = 3;
-		assertEqual(estimatedValue, greentea.MeasureMetric.measureCyclomatic(new testexample.SimpleTest().cycle()));
+		IMethod dummy = null;
+		assertEquals(estimatedValue, Metric.measureCyclomatic(dummy));
 	}
 	@Test
 	public void MaintainIndexTest() {
 		double estimatedValue = 85.7;
-		assertEqual(estimatedValue, greentea.MeasureMetric.measureCoupling(new testexample.SimpleTest().cycle()));
+		IMethod dummy = null;
+		assertEquals(estimatedValue, Metric.measureMaintain(dummy));
 	}
 }
