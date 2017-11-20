ece2bb8f0899d2f27e406ce313347f148ae6d4b4
Park Junsu <jspak3@postech.ac.kr>
2017-11-20 23:21:55 +0900

    Implemented T_S06

--- GreenTea/src/greentea/Metric.java
+++ GreenTea/src/greentea/Metric.java
@@ -1,5 +1,38 @@
 package greentea;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import org.eclipse.jdt.core.*;
+import org.eclipse.jdt.core.search.*;
+
+
 public class Metric {
-	static final int numberOfMetric = 3;
+	public Metric() {
+		
+	}
+	
+	static public int measureLOC(IMethod method) {
+		// T_S01
+		return 0;
+	}
+	
+	static public double measureHalstead(IMethod method) {
+		// T_S02
+		return 0;
+	}
+	
+	static public int measureCyclomatic(IMethod method) {
+		// to T_S03
+		return 0;
+	}
+	
+	static public double measureMaintain(IMethod method) {
+		return Math.max(0, (171
+				- 5.2 * Math.log(Metric.measureHalstead(method))
+				- 0.23 * Metric.measureCyclomatic(method)
+				- 16.2 * Math.log(Metric.measureLOC(method))
+				) * 100 / 171 );
+	}
 }
