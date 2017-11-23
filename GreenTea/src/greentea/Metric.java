407c7154ff3914c225b052dc9d0655f327b31329
HanSeoupByun <qusgkstjq15@postech.ac.kr>
2017-11-23 17:50:23 +0900

    Count arithOperators with jaechang

--- GreenTea/src/greentea/Metric.java
+++ GreenTea/src/greentea/Metric.java
@@ -5,59 +5,98 @@ import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class Metric {
-public static class HalsteadVolume{	
-		
-		private static final ArrayList<String> ops = new ArrayList<String>(Arrays.asList("abstract","continue","for","new","switch","assert","default","goto","package","synchronized"
-				,"boolean","do","if","private","this","break","double","implements","protected","throw","byte"
-				,"else","import","public","throws","case","enum","instanceof","return","transient","catch","extends"
-				,"int","short","try","char","final","interface","static","void","class","finally","long","strictfp"
-				,"volatile","const","float","native","super","while"
-				,"+","++","-","--","*",".",";","/","%","!",">","<",">=","<=","==","=",":"
-				,"{","}","(",")","[","]","<",">"));
-		
-		private static final String arithOps="++--*.;/%!><>=<==:{}()[]<>";
-		
-		private int operatorCNT =0, operandCNT =0, operatorDist =0, operandDist=0;
-		
-		public String rmStringOperand(String source) {
-			String[] StrOperandRemoved=source.split("\"");
-			String rmStringOperandSource="";
+	public static class HalsteadVolume{	
 			
-			//System.out.println(StrTokens);
-			for(int i=0; i<StrOperandRemoved.length; i++) {
-				if(i%2==1)
-					operandCNT++;
-				else
-					rmStringOperandSource+=StrOperandRemoved[i];
+			private static final ArrayList<String> keyword = new ArrayList<String>(Arrays.asList(
+					"abstract","continue","for","new","switch","assert","default","goto","package","synchronized"
+					,"boolean","do","if","private","this","break","double","implements","protected","throw","byte"
+					,"else","import","public","throws","case","enum","instanceof","return","transient","catch","extends"
+					,"int","short","try","char","final","interface","static","void","class","finally","long","strictfp"
+					,"volatile","const","float","native","super","while"
+					));
+			
+			private static final String[] ops = {
+					"++","--","*",".",";","/","%","!",">","<",">=","<=","==",":"
+					,"{","}","(",")","[","]","<",">"};
+		
+			private static final String arithOps="++--*.;/%!><>=<==:{}()[]<>";
+			
+			private int operatorCNT =0, operandCNT =0, operatorDist =0, operandDist=0;
+			
+			
+			public int countWord(String source, String findStr) {
+				int lastIndex = 0;
+				int count = 0;
+
+				while(lastIndex != -1){
+
+				    lastIndex = source.indexOf(findStr,lastIndex);
+
+				    if(lastIndex != -1){
+				        count ++;
+				        lastIndex += findStr.length();
+				    }
+				}
+				return count;
 			}
-			return rmStringOperandSource;
+			
+			
+			public void arithOpCheck(String source) {
+				for(int i=0; i<ops.length; i++) {
+					if(source.contains(ops[i])) {
+						operatorDist++;
+						operatorCNT+=countWord(source, ops[i]);
+					}
+				}
+			}
+			
+			
+			public String rmStringOperand(String source) {
+				String[] StrOperandRemoved=source.split("\"");
+				String rmStringOperandSource="";
+				
+				//System.out.println(StrTokens);
+				for(int i=0; i<StrOperandRemoved.length; i++) {
+					if(i%2==1)
+						operandCNT++;
+					else
+						rmStringOperandSource+=StrOperandRemoved[i];
+				}
+				return rmStringOperandSource;
+			}
 		}
-	}
-	
-	public static void commit01() {
-		final String source=	"public class practice {"
-				+"\npublic static void main(String[] args) {"
-				+	"\nString data = \"happy new     year   djkdl!\";"
-				+	"\nStringTokenizer strToken = new StringTokenizer(data,\" \");"
-				+	"\nSystem.out.println(strToken.countTokens());"
-				+	"\nwhile(strToken.hasMoreTokens()) {"
-				+		"\nSystem.out.println(strToken.nextToken());"
-				+	"\n}"
-				+	"\nSystem.out.println(\"well done!\");"
-				+"\n}"
-			+"\n}";
 		
-		HalsteadVolume hals = new HalsteadVolume();
+		public static void implementation() {
+			final String source= "public class practice {"
+					+"\npublic static void main(String[] args) {"
+					+	"\nString data = \"happy new     year   djkdl!\";"
+					+	"\nStringTokenizer strToken = new StringTokenizer(data,\" \");"
+					+	"\nSystem.out.println(strToken.countTokens());"
+					+	"\nwhile(strToken.hasMoreTokens()) {"
+					+		"\nSystem.out.println(strToken.nextToken());"
+					+	"\n}"
+					+	"\nSystem.out.println(\"well done!\");"
+					+"\n}"
+				+"\n}";
+			
+			HalsteadVolume hals = new HalsteadVolume();
+			
+			hals.arithOpCheck(source);
+			System.out.println(hals.operatorCNT);
+			System.out.println(hals.operatorDist);
+			
+			String tmpSource1=hals.rmStringOperand(source);
+			
+			StringTokenizer StrTokens = new StringTokenizer(tmpSource1,HalsteadVolume.arithOps+", \n");
+			
+			System.out.println(StrTokens.countTokens());
+			while(StrTokens.hasMoreTokens()) { System.out.println(StrTokens.nextToken()); }
+			System.out.println(hals.operandCNT);
+			
+			
+		}
 		
-		String tmpSource1=hals.rmStringOperand(source);
-		
-		StringTokenizer StrTokens = new StringTokenizer(tmpSource1,HalsteadVolume.arithOps+", \n");
-		
-		System.out.println(StrTokens.countTokens());
-		while(StrTokens.hasMoreTokens()) { System.out.println(StrTokens.nextToken()); }
-	}
-	
-	public static void main(String[] args) {
-		commit01();
-	}
+		public static void main(String[] args) {
+			implementation();
+		}
 }
