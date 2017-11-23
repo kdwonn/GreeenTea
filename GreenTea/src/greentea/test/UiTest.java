de24d83cc1c1d84583e364dc0976433ce3a14830
JungHwan Woo <woowong@postech.ac.kr>
2017-11-23 17:59:57 +0900

    Add test case for checking tutorial open button | with @wookiekim

--- GreenTea/src/greentea/test/UiTest.java
+++ GreenTea/src/greentea/test/UiTest.java
@@ -3,6 +3,7 @@ package greentea.test;
 import static org.junit.Assert.*;
 
 import org.eclipse.swtbot.eclipse.finder.SWTWorkbenchBot;
+import org.eclipse.swtbot.eclipse.finder.widgets.SWTBotView;
 import org.eclipse.swtbot.swt.finder.junit.SWTBotJunit4ClassRunner;
 import org.eclipse.swtbot.swt.finder.widgets.SWTBotLabel;
 import org.eclipse.swtbot.swt.finder.widgets.SWTBotMenu;
@@ -23,49 +24,31 @@ public class UiTest {
 	public static void initBot() {
 		// init swtbot
 		bot = new SWTWorkbenchBot();
-		bot.menu("File").menu("New").menu("Project...").click();
-		
-		SWTBotShell dialog = bot.shell("New Project");
-	    dialog.activate();
-	    
-	    bot.tree().expandNode("Java").select("Java Project");
-	    bot.button("Next >").click();
-	    bot.textWithLabel("Project name:").setText("Demo");
-	    bot.button("Finish").click();
-	    
-	    dialog = bot.shell("Open Associated Perspective?");
-	    bot.button("No").click();
+		bot.viewByTitle("Welcome").close();
 	}
-	@Test
-	public void testPulginStart() {
-		// Start plugin and check ecisting project appears in tree view
+
+	@AfterClass
+	public static void afterClass() {
+		bot.sleep(2000);
+		bot.resetWorkbench();
+	}
+	
+	/*
+	 * Test case for checking tutorial open button
+	 * Tutorial open button is placed in the toolbar of this plugin.
+	 */ 
+	@Test 
+	public void testTutorialOpen() {
 		bot.menu("Window").menu("Show View").menu("Other...").click();
-		SWTBotShell dialog = bot.shell("Show View");
-	    dialog.activate();
-	    bot.tree().expandNode("CSED332").select("Package Overview");
-	    bot.button("Open").click();
-	    
-	    assertEquals("Demo", bot.viewByTitle("GreenTea").bot().tree().getTreeItem("Demo").getText());
+		bot.tree().expandNode("Green Tea").getNode("Green Tea").select();
+		bot.button("Open").click();
+		
+		SWTBotView view = bot.viewByTitle("Green Tea");
+		assertEquals("Open Tutorial", view.getToolbarButtons().get(0).getToolTipText());
+		view.getToolbarButtons().get(0).click();
+		view.bot().toolbarButton().click();
+		
+		assertEquals("Tutorial", bot.activeView().getTitle());
 	}
-	@Test
-	public void testAddPackageClass() {
-		// Add new package and class in workspace and check tree view is updated
-		bot.menu("File").menu("New").menu("Package").click();
-	    SWTBotShell dialog = bot.shell("New java Package");
-	    dialog.activate();
-	    bot.textWithLabel("Source folder:").setText("Demo/src");
-	    bot.textWithLabel("Name:").setText("demoPackage");
-	    bot.button("Finish").click();
-	    
-	    bot.menu("File").menu("New").menu("File").click();
-	    dialog = bot.shell("New File");
-	    dialog.activate();
-	    bot.textWithLabel("Enter or select the parent folder:").setText("Demo/src/demoPackage");
-	    bot.textWithLabel("File name:").setText("simple.java");
-	    bot.button("Finish").click();
-	    
-	    
-		assertEquals("demoPackage", bot.tree().getTreeItem("homework5").getText());
-		assertEquals("simple.java", bot.tree().getTreeItem("simple.java").getText());
-	}
+	
 }
