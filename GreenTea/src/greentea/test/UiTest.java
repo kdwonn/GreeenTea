1f35f92c988f7e702407322386980246a85c9a2b
JungHwan Woo <woowong@postech.ac.kr>
2017-11-23 18:42:56 +0900

    Add test case for log button | with @wookiekim

--- GreenTea/src/greentea/test/UiTest.java
+++ GreenTea/src/greentea/test/UiTest.java
@@ -7,14 +7,7 @@ import java.io.File;
 import org.eclipse.swtbot.eclipse.finder.SWTWorkbenchBot;
 import org.eclipse.swtbot.eclipse.finder.widgets.SWTBotView;
 import org.eclipse.swtbot.swt.finder.junit.SWTBotJunit4ClassRunner;
-import org.eclipse.swtbot.swt.finder.widgets.SWTBotLabel;
-import org.eclipse.swtbot.swt.finder.widgets.SWTBotMenu;
-import org.eclipse.swtbot.swt.finder.widgets.SWTBotShell;
-import org.eclipse.swtbot.swt.finder.widgets.SWTBotText;
-import org.eclipse.swtbot.swt.finder.widgets.SWTBotTree;
-import org.eclipse.swtbot.swt.finder.widgets.SWTBotTreeItem;
 import org.junit.AfterClass;
-import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -54,21 +47,40 @@ public class UiTest {
 	}
 	
 	/*
-	 * Test case for checking text report function and report md exis.
+	 * Test case for checking text report function and creating report md file.
 	 * Report button is placed in the toolbar of this plugin.
 	 */
 	@Test
-	public void testOpenTextReport() {
+	public void testMakeTextReport() {
 		bot.menu("Window").menu("Show View").menu("Other...").click();
 		bot.tree().expandNode("Green Tea").getNode("Green Tea").select();
 		bot.button("Open").click();
 		
 		SWTBotView view = bot.viewByTitle("Green Tea");
-		assertEquals("Open Text report", view.getToolbarButtons().get(1).getToolTipText());
+		assertEquals("Make Text report", view.getToolbarButtons().get(1).getToolTipText());
 		view.getToolbarButtons().get(1).click();
 		view.bot().toolbarButton().click();
 		
 		File report = new File("text_report.md");
+		assertTrue(report.exists());
+	}
+	
+	/*
+	 * Test case for checking logging metrics function
+	 * Log button is placed in the toolbar of this plugin.
+	 */
+	@Test
+	public void testMakeLog() {
+		bot.menu("Window").menu("Show View").menu("Other...").click();
+		bot.tree().expandNode("Green Tea").getNode("Green Tea").select();
+		bot.button("Open").click();
+		
+		SWTBotView view = bot.viewByTitle("Green Tea");
+		assertEquals("Make Log file", view.getToolbarButtons().get(2).getToolTipText());
+		view.getToolbarButtons().get(2).click();
+		view.bot().toolbarButton().click();
+		
+		File report = new File("metric_log.txt");
 		assertTrue(report.exists());
 	}
 }
