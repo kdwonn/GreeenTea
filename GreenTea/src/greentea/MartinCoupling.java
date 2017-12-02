d8ad0cc37a1b7d09ab31400e9f61c1c1be3d75b6
Park Junsu <jspak3@postech.ac.kr>
2017-12-02 21:31:58 +0900

    Reimplemented Martin Efferent Coupling metric | with kdwon

--- GreenTea/src/greentea/MartinCoupling.java
+++ GreenTea/src/greentea/MartinCoupling.java
@@ -93,6 +93,19 @@ public class MartinCoupling {
 	private int CalcEfferentCoupling(IPackageFragment targetPackage) {
 		int result = 0;
 
+		try {
+			SearchEngine searchEngine = new SearchEngine();
+			IJavaSearchScope scope = SearchEngine.createJavaSearchScope(new IJavaElement[] {targetPackage});
+			SearchPattern pattern = SearchPattern.createPattern("*", IJavaSearchConstants.PACKAGE, IJavaSearchConstants.REFERENCES, SearchPattern.R_PATTERN_MATCH);
+			EfferentRequestor requestor = new EfferentRequestor();
+			SearchParticipant[] participant = new SearchParticipant[] { SearchEngine.getDefaultSearchParticipant() };
+			searchEngine.search(pattern, participant, scope, requestor, null);
+			result = requestor.getResult();
+		} catch (CoreException e) {
+			e.printStackTrace();
+			result = -1;
+		}
+		
 		return result;
 	}
 
@@ -106,34 +119,6 @@ public class MartinCoupling {
 
 	public double getInstability() {
 		return instability;
-	}
-	
-	/*
-	 * Test AfferentCoupling testexample.Bank with Account.java
-	 * Expected result = 0
-	 */
-	public static int testCalcAfferentCoupling() {
-		int result = 0;
-		
-		return result;
-	}
-	
-	/*
-	 * Test EfferentCoupling testexample.Bank with Account.java
-	 * Expected result = 1
-	 */
-	public static int testCalcEfferentCoupling() {
-		int result = 0;
-		
-		return result;
-	}
-	
-	/*
-	 * Test Instability testexample.Bank with Account.java
-	 * Expected result = 1
-	 */
-	public static double testInstability() {
-		return testCalcEfferentCoupling() / (testCalcEfferentCoupling() + testCalcAfferentCoupling());
 	}
 	
 	public static class AfferentRequestor extends SearchRequestor {
@@ -169,269 +154,64 @@ public class MartinCoupling {
 			result = results.size();
 		}
 	}
-}
-
-class testResources {
-	public static String testoutersrc = 
-			"package testexample;\r\n" + 
-			"\r\n" + 
-			"/**\r\n" + 
-			" *  This class contains the information needed to represent an account.\r\n" + 
-			" *  Account should have information such as owner's name, account number and balance. \r\n" + 
-			" */\r\n" + 
-			"abstract class Account {\r\n" + 
-			"	int accountNumber;\r\n" + 
-			"	protected double balance;\r\n" + 
-			"	String owner;\r\n" + 
-			"\r\n" + 
-			"	/**\r\n" + 
-			"	 *  This constructor is used when account is created in \"Bank\" object.\r\n" + 
-			"	 * \r\n" + 
-			"	 *  @param newNumber the new account number \r\n" + 
-			"     *  @param name the name of account owner\r\n" + 
-			"     *  @param initialFund initial balance when an account is created\r\n" + 
-			"	 */	\r\n" + 
-			"	Account(int newNumber, String name, double initialFund) {\r\n" + 
-			"		accountNumber = newNumber;\r\n" + 
-			"		owner = name;\r\n" + 
-			"		balance = initialFund;\r\n" + 
-			"	}\r\n" + 
-			"	\r\n" + 
-			"	/**\r\n" + 
-			"	 *  This function takes elapsed date and updates account balance accordingly.\r\n" + 
-			"	 *  \r\n" + 
-			"	 *  @param elapsedDate elapsed date\r\n" + 
-			"	 */\r\n" + 
-			"	abstract void updateBalance(int elapsedDate);\r\n" + 
-			"	\r\n" + 
-			"	/**\r\n" + 
-			"	 *  Returns the account balance.\r\n" + 
-			"	 *  \r\n" + 
-			"	 *  @return the balance\r\n" + 
-			"	 */\r\n" + 
-			"	double getBalance() {\r\n" + 
-			"		return balance;\r\n" + 
-			"	}\r\n" + 
-			"\r\n" + 
-			"	\r\n" + 
-			"	/**\r\n" + 
-			"	 *  Add as much money as a given amount factor to the given account.\r\n" + 
-			"	 *  \r\n" + 
-			"	 *  @param amount deposit amount\r\n" + 
-			"	 */\r\n" + 
-			"	void deposit(double amount) {\r\n" + 
-			"		balance += amount;\r\n" + 
-			"		//음수인 경우 고려X\r\n" + 
-			"	}\r\n" + 
-			"	\r\n" + 
-			"	/**\r\n" + 
-			"	 *  Withdraw as much money as a given amount factor to the given account.\r\n" + 
-			"	 *  You have to raise exception in case when the balance of the account\r\n" + 
-			"	 *  is smaller than the amount of money that you want to withdraw.\r\n" + 
-			"	 *  Please use NegativeException we gave you.\r\n" + 
-			"	 *  \r\n" + 
-			"	 *  @param amount withdraw amount \r\n" + 
-			"	 */\r\n" + 
-			"	void withdraw(double amount) throws NegativeException {\r\n" + 
-			"		if (balance < amount ) {\r\n" + 
-			"			throw new NegativeException();\r\n" + 
-			"		}\r\n" + 
-			"		else {\r\n" + 
-			"			balance -= amount;\r\n" + 
-			"		}\r\n" + 
-			"	}\r\n" + 
-			"}";
 	
-	public static String testinnersrc = 
-			"package testexample;\r\n" + 
-			"\r\n" + 
-			"import java.util.Map;\r\n" + 
-			"import java.util.HashMap;\r\n" + 
-			"import java.util.Collection;\r\n" + 
-			"\r\n" + 
-			"/**\r\n" + 
-			" * The Bank class has all account information that have been created so far. The\r\n" + 
-			" * account number starts at 100000. When the first person creates an account,\r\n" + 
-			" * his account number is 100000 and the account number of the second person who\r\n" + 
-			" * creates an account is 100001.\r\n" + 
-			" */\r\n" + 
-			"public class Bank {\r\n" + 
-			"	String name;\r\n" + 
-			"	int accNumCounter;\r\n" + 
-			"	Map<Integer, Account> accs;\r\n" + 
-			"	Map<String, Account> accsByName;\r\n" + 
-			"\r\n" + 
-			"	/**\r\n" + 
-			"	 * Creates a new bank with the given name.\r\n" + 
-			"	 * \r\n" + 
-			"	 * @param bankname\r\n" + 
-			"	 *            the name of bank\r\n" + 
-			"	 */\r\n" + 
-			"	public Bank(String bankname) {\r\n" + 
-			"		name = bankname;\r\n" + 
-			"		accNumCounter = 0;\r\n" + 
-			"		accs = new HashMap<Integer, Account>();\r\n" + 
-			"		accsByName = new HashMap<String, Account>();\r\n" + 
-			"	}\r\n" + 
-			"\r\n" + 
-			"	/**\r\n" + 
-			"	 * Find account with given account number. You have to raise exception when not\r\n" + 
-			"	 * found. Please use NotFoundException we gave you.\r\n" + 
-			"	 * \r\n" + 
-			"	 * @param accountNum\r\n" + 
-			"	 *            looking account number\r\n" + 
-			"	 * @return requested account\r\n" + 
-			"	 */\r\n" + 
-			"	Account findAccount(int accountNum) throws NotFoundException {\r\n" + 
-			"		Account account = accs.get(accountNum);\r\n" + 
-			"		if (account == null) {\r\n" + 
-			"			throw new NotFoundException();\r\n" + 
-			"		}\r\n" + 
-			"		return account;\r\n" + 
-			"	}\r\n" + 
-			"\r\n" + 
-			"	/**\r\n" + 
-			"	 * Find account by owner's name. You have to raise exception when not found.\r\n" + 
-			"	 * Please use NotFoundException we gave you. If multiple accounts are found by\r\n" + 
-			"	 * the given name, return the account with the lowest account number.\r\n" + 
-			"	 * \r\n" + 
-			"	 * @param name\r\n" + 
-			"	 *            looking account owner's name\r\n" + 
-			"	 * @return requested account\r\n" + 
-			"	 */\r\n" + 
-			"	Account findAccountByName(String name) throws NotFoundException {\r\n" + 
-			"		Account account = accsByName.get(name);\r\n" + 
-			"		if (account == null) {\r\n" + 
-			"			throw new NotFoundException();\r\n" + 
-			"		}\r\n" + 
-			"		return account;\r\n" + 
-			"	}\r\n" + 
-			"\r\n" + 
-			"	/**\r\n" + 
-			"	 * Create a new account with the given owner's name, the type of account. and\r\n" + 
-			"	 * initial balance.\r\n" + 
-			"	 * \r\n" + 
-			"	 * @param name\r\n" + 
-			"	 *            owner name\r\n" + 
-			"	 * @param accType\r\n" + 
-			"	 *            kind of account\r\n" + 
-			"	 * @param initial\r\n" + 
-			"	 *            initial balance of account\r\n" + 
-			"	 * @return the created account\r\n" + 
-			"	 */\r\n" + 
-			"	Account createAccount(String name, ACCTYPE accType, double initial) {\r\n" + 
-			"		// Not Complete\r\n" + 
-			"		Account newAccount;\r\n" + 
-			"		if (accType == ACCTYPE.SIMPLE)\r\n" + 
-			"			newAccount = new SimpleInterest(accNumCounter, name, initial);\r\n" + 
-			"		else\r\n" + 
-			"			newAccount = new CompoundInterest(accNumCounter, name, initial);\r\n" + 
-			"		accs.put(accNumCounter, newAccount);\r\n" + 
-			"		accsByName.put(name, newAccount);\r\n" + 
-			"		accNumCounter++;\r\n" + 
-			"		return newAccount;\r\n" + 
-			"	}\r\n" + 
-			"\r\n" + 
-			"	/**\r\n" + 
-			"	 * Find the account which has the highest balance. You have to raise exception\r\n" + 
-			"	 * when not found. Please use EmptyException we gave you. If there are multiple\r\n" + 
-			"	 * accounts with the same maximum balance, return the account with the lowest\r\n" + 
-			"	 * account number.\r\n" + 
-			"	 */\r\n" + 
-			"	Account maxBalance() throws EmptyException {\r\n" + 
-			"		if (accNumCounter == 0) {\r\n" + 
-			"			throw new EmptyException();\r\n" + 
-			"		}\r\n" + 
-			"		double max = -1;\r\n" + 
-			"		int maxNum = -1;\r\n" + 
-			"		for (int i = 0; i < accNumCounter; i++) {\r\n" + 
-			"			try {\r\n" + 
-			"				Account acci = findAccount(i);\r\n" + 
-			"				if (acci.getBalance() > max) {\r\n" + 
-			"					maxNum = i;\r\n" + 
-			"					max = acci.getBalance();\r\n" + 
-			"				}\r\n" + 
-			"			} catch (NotFoundException e) {\r\n" + 
-			"			}\r\n" + 
-			"		}\r\n" + 
-			"		return accs.get(maxNum);\r\n" + 
-			"	}\r\n" + 
-			"\r\n" + 
-			"	/**\r\n" + 
-			"	 * Transfer the money as much as given amount factor from src account to dst\r\n" + 
-			"	 * account. You have to raise exception in case when the balance of src account\r\n" + 
-			"	 * is smaller than the amount to be sent. Please use NegativeException we gave\r\n" + 
-			"	 * you. You cannot send money from one bank to another.\r\n" + 
-			"	 * \r\n" + 
-			"	 * @param src\r\n" + 
-			"	 *            the account to send money\r\n" + 
-			"	 * @param dst\r\n" + 
-			"	 *            the account to receive money\r\n" + 
-			"	 * @param amount\r\n" + 
-			"	 *            amount of money to send\r\n" + 
-			"	 */\r\n" + 
-			"	void transfer(Account src, Account dst, double amount) throws NegativeException {\r\n" + 
-			"		src.withdraw(amount);\r\n" + 
-			"		dst.deposit(amount);\r\n" + 
-			"	}\r\n" + 
-			"\r\n" + 
-			"	public static void main(String[] args) {\r\n" + 
-			"		int errCtr = 0;\r\n" + 
-			"		Bank wb = new Bank(\"SDBank\");\r\n" + 
-			"\r\n" + 
-			"		/**\r\n" + 
-			"		 * Test case 1. Checking correctness of simple interest accruement.\r\n" + 
-			"		 */\r\n" + 
-			"		Account s = wb.createAccount(\"Thomas\", ACCTYPE.SIMPLE, 100000.);\r\n" + 
-			"		double expected = 100000.0 * (1 + SimpleInterest.interest * 20);\r\n" + 
-			"		s.updateBalance(20);\r\n" + 
-			"		double result = s.getBalance();\r\n" + 
-			"		if (expected != result) {\r\n" + 
-			"			System.out.println(\"Your implementation is certainly wrong (#1)\");\r\n" + 
-			"			errCtr++;\r\n" + 
-			"		}\r\n" + 
-			"\r\n" + 
-			"		/**\r\n" + 
-			"		 * Test case 2. Checking findAccount method's exception raising.\r\n" + 
-			"		 */\r\n" + 
-			"		try {\r\n" + 
-			"			wb.findAccount(100001);\r\n" + 
-			"			System.out.println(\"Your implementation is certainly wrong (#2)\");\r\n" + 
-			"			errCtr++;\r\n" + 
-			"		} catch (NotFoundException e) {\r\n" + 
-			"		}\r\n" + 
-			"\r\n" + 
-			"		/**\r\n" + 
-			"		 * Test case 3. Checking withdraw method's exception raising.\r\n" + 
-			"		 */\r\n" + 
-			"		try {\r\n" + 
-			"			s.withdraw(9999999999.9);\r\n" + 
-			"			System.out.println(\"Your implementation is certainly wrong (#3)\");\r\n" + 
-			"			errCtr++;\r\n" + 
-			"		} catch (NegativeException e) {\r\n" + 
-			"		}\r\n" + 
-			"\r\n" + 
-			"		/**\r\n" + 
-			"		 * Test case 4. Checking maxBalance method's exception raising.\r\n" + 
-			"		 */\r\n" + 
-			"		Bank eb = new Bank(\"EmptyBank\");\r\n" + 
-			"		try {\r\n" + 
-			"			eb.maxBalance();\r\n" + 
-			"			System.out.println(\"Your implementation is certainly wrong (#4)\");\r\n" + 
-			"			errCtr++;\r\n" + 
-			"		} catch (EmptyException e) {\r\n" + 
-			"		}\r\n" + 
-			"\r\n" + 
-			"		if (errCtr > 0) {\r\n" + 
-			"			System.out.println(Integer.toString(errCtr) + \" error(s) found.\");\r\n" + 
-			"			return;\r\n" + 
-			"		}\r\n" + 
-			"\r\n" + 
-			"		System.out.println(\"Your implementation looks fine at a glance, but there\");\r\n" + 
-			"		System.out.println(\"is no guarantee that your implementation is correct.\");\r\n" + 
-			"		System.out.println(\"It is your job to ensure correctness by thorough testing.\");\r\n" + 
-			"	}\r\n" + 
-			"\r\n" + 
-			"}\r\n" + 
-			"";
+	public static class EfferentRequestor extends SearchRequestor {
+		int result = 0;
+		Set<String> results = null;
+		Set<String> packages = null;
+
+		public EfferentRequestor() {
+		}
+
+		public int getResult() {
+			return result;
+		}
+
+		@Override
+		public void beginReporting() {
+			results = new HashSet<String>();
+			packages = new HashSet<String>();
+		}
+
+		private String getPackageName(IJavaElement enclosingElement, int start, int end) {
+			if (enclosingElement.getElementType() == IJavaElement.IMPORT_DECLARATION) {
+				String name = enclosingElement.getElementName();
+				int lastDot = name.lastIndexOf('.');
+				return name.substring(0, lastDot);
+			}
+
+			ICompilationUnit unit = (ICompilationUnit) enclosingElement.getAncestor(IJavaElement.COMPILATION_UNIT);
+
+			try {
+				String source = unit.getSource();
+				return source.substring(start, end);
+			} catch (JavaModelException e) {
+				return null;
+			}
+		}
+
+		@Override
+		public void acceptSearchMatch(SearchMatch match) throws CoreException {
+			int start = match.getOffset();
+			int end = start + match.getLength();
+			
+			if (match.getElement() != null) {
+				try {
+					String name = getPackageName((IJavaElement) match.getElement(), start, end);
+					
+					if (!name.startsWith("java")) {
+						results.add(match.getResource().getFullPath().toString());
+						packages.add(name);
+					}
+				} catch (StringIndexOutOfBoundsException x) {
+					
+				}
+			}
+		}
+
+		@Override
+		public void endReporting() {
+			result = results.size();
+		}
+	}
 }
