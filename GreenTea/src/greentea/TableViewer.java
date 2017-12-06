a7011b57f63559115812bf0d1e1c4cd9c86a451d
lngsg <lngsg@postech.ac.kr>
2017-12-07 01:24:12 +0900

    Move viewer code and add filter w/ HanSeoupByun

--- GreenTea/src/greentea/TableViewer.java
+++ GreenTea/src/greentea/TableViewer.java
@@ -3,9 +3,5 @@ package greentea;
 import org.eclipse.jface.viewers.*;
 
 public class TableViewer {
-	private org.eclipse.jface.viewers.TableViewer viewer;
 	
-	public org.eclipse.jface.viewers.TableViewer getViewer() {
-		return viewer;
-	}
 }
