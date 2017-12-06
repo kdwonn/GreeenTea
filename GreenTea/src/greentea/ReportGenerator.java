29ad82c037beecfbac8780b72925ea684b818c9b
Kim Jaechang <jaechang@postech.ac.kr>
2017-12-07 02:49:22 +0900

    add generateMarkdownString method with wookiekim

--- GreenTea/src/greentea/ReportGenerator.java
+++ GreenTea/src/greentea/ReportGenerator.java
@@ -1,5 +1,16 @@
 package greentea;
 
-public class ReportGenerator {
+import net.steppschuh.markdowngenerator.table.Table;
+import net.steppschuh.markdowngenerator.text.heading.Heading;
 
+public class ReportGenerator {
+	public String generateMarkdownString() {
+		StringBuilder sb = new StringBuilder().append(new Heading("Metric Report", 1)).append("\n");
+		
+		Table.Builder tableBuilder = new Table.Builder().withAlignment(Table.ALIGN_LEFT);
+		//TODO : build table
+		
+		sb = sb.append(tableBuilder.build()).append("\n");
+		return sb.toString();
+	}
 }
