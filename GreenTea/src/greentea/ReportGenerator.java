f357cadc2c6a5228aa97be2d3305894f6b9a94f0
Kim Jaechang <jaechang@postech.ac.kr>
2017-12-13 19:38:44 +0900

    modify format with wookiekim

--- GreenTea/src/greentea/ReportGenerator.java
+++ GreenTea/src/greentea/ReportGenerator.java
@@ -13,19 +13,19 @@ public class ReportGenerator {
 	public static String generateMarkdownString() {
 		StringBuilder sb = new StringBuilder().append("Metric Report").append("\n").append("====================").append("\n");
 		
-		String table = "| Name | Lines of Code | Halstead Volume | Cyclomatec Complexity | Martin's Coupling | Maintainablity Index";
-		table = table += "| ------- | ------- | ------- | ------- | ------- | ------- |";
+		String table = "| Name | Lines of Code | Halstead Volume | Cyclomatec Complexity | Martin's Coupling | Maintainablity Index |\n";
+		table = table += "| ------- | ------- | ------- | ------- | ------- | ------- |\n";
 		
 		for(String projectName:ProjectAnalyser.getProjectNames())
 			for(String packageName:ProjectAnalyser.getPackageNames(projectName)) {
-				table += String.format("| %s |  |  |  | %s |  |", projectName + "." + packageName,
+				table += String.format("| %s |  |  |  | %s |  |\n", projectName + "." + packageName,
 						String.valueOf(Metric.measureMartinInstability(projectName, packageName)));
 				for(String className:ProjectAnalyser.getClassNames(projectName, packageName)) {
-					table += String.format("| %s | %s |  |  |  |  |", projectName + "." + packageName+ "." + className,
+					table += String.format("| %s | %s |  |  |  |  |\n", projectName + "." + packageName+ "." + className,
 							String.valueOf(Metric.measureLOC(projectName, packageName, className)));
 					for(String methodName:ProjectAnalyser.getMethodNames(projectName, packageName, className)) {
 						IMethod method = ProjectAnalyser.getIMethod(projectName, packageName, className, methodName);
-						table += String.format("| %s | %s | %s | %s |  | %s |", projectName + "." + packageName + "." + className + "." + methodName,
+						table += String.format("| %s | %s | %s | %s |  | %s |\n", projectName + "." + packageName + "." + className + "." + methodName,
 								String.valueOf(Metric.measureLOC(method)),
 								String.valueOf(Metric.measureHalstead(projectName, packageName, className, methodName)),
 								String.valueOf(Metric.measureCyclomatic(projectName, packageName, className, methodName)),
@@ -41,8 +41,10 @@ public class ReportGenerator {
 	public static void generateReport(String filename) {
 		try {
 			File file = new File(filename) ;
+			file.deleteOnExit();
 			FileWriter fw = new FileWriter(file, true);
-	        fw.write(generateMarkdownString());
+			String report = generateMarkdownString();
+	        fw.write(report);
 	        fw.flush();
 	        fw.close();
 		} catch (IOException e) {
